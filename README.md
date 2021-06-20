4cc494783c8913fdbc4f531c2116fb2e61f2abfc
coderj001 <amirajubolchi001@gmail.com>
2021-06-20 18:25:28 +0530

    image upload is config

---
+++ README.md
@@ -0,0 +1,2 @@
+## FullStack Blog (MERN)
+
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
@@ -21,6 +22,20 @@ mongoose
   })
   .then(console.log("Connect To Mongodb"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File is uploaded.");
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
